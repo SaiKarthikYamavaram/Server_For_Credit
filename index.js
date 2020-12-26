8752262282abb51f907dd259b4d41f6f72c5186b
sai-yamavaram <saikarthiksep01@gmail.com>
2020-12-26 21:01:40 +0530

    added cors

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ const bodyParser = require("body-parser");
 const app = express();
 const checker = require("./src/verifiyLiscence");
 const cors = require("cors");
+const morgan = require("morgan");
 
+app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/", cors(), (req, res, next) => {
+app.use(cors());
+app.use("/", (req, res, next) => {
 	next();
 });
 
-app.get("/", cors(), (req, res, next) => {
+app.get("/", (req, res, next) => {
 	res.send("Hello");
 });
 
-app.post("/license", cors(), async (req, res, next) => {
-	console.log("data: ", req.body.regNo);
+app.post("/license", async (req, res, next) => {
+	console.log("data: ", req.body);
 	const value = await checker(req.body.regNo);
 	res.send(value);
 });
 
-app.use("/", cors(), (req, res, next) => {
+app.use("/", (req, res, next) => {
 	res.send("<h1> first midleware: Hello Tutorials Point </h1>");
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("server running "));
+app.listen(port, () => console.log("server running at", port));
