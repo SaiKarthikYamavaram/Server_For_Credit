d0e375686e67819ee9805c17eb62a55f845814f1
sai-yamavaram <saikarthiksep01@gmail.com>
2020-12-27 10:07:49 +0530

    working model with try catch

--- src/verifiyLiscence.js
+++ src/verifiyLiscence.js
@@ -12,32 +12,30 @@ const checker = async (no) => {
 			"https://tgtransport.net/TGCFSTONLINE/Reports/OnlineLicenceSearch.aspx",
 			{ waitUntil: "domcontentloaded" }
 		);
-		try {
-			await page.evaluate(
-				(val) =>
-					(document.querySelector("#ctl00_OnlineContent_txtDlNo").value = val),
-				no
-			);
-			// await page.waitForNavigation();
 
-			await page.evaluate(async () => {
-				await document.querySelector("#ctl00_OnlineContent_btnGet").click();
-			});
+		await page.evaluate(
+			(val) =>
+				(document.querySelector("#ctl00_OnlineContent_txtDlNo").value = val),
+			no
+		);
+		// await page.waitForNavigation();
 
-			await page.waitForNavigation();
-			value = await page.evaluate(async () =>
-				Array.from(
-					document.querySelector("#ctl00_OnlineContent_gvLic").children[0]
-						.children[1].children
-				)
-					.filter((i) => i.textContent.trim().length !== 0)
-					.map((i) => {
-						return i.textContent;
-					})
-			);
-		} catch (error) {
-			console.log(error.message);
-		}
+		await page.evaluate(async () => {
+			await document.querySelector("#ctl00_OnlineContent_btnGet").click();
+		});
+
+		await page.waitForNavigation();
+		value = await page.evaluate(async () =>
+			Array.from(
+				document.querySelector("#ctl00_OnlineContent_gvLic").children[0]
+					.children[1].children
+			)
+				.filter((i) => i.textContent.trim().length !== 0)
+				.map((i) => {
+					return i.textContent;
+				})
+		);
+
 		// await page.waitForNavigation();
 		await browser.close();
 
